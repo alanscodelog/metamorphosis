ba5e9fecfa3ab73517eff1611191da3ff12df145
Alan North <alanscodelog@gmail.com>
2025-09-27 11:22:38 -0300

    deps: updated typedoc to esm

---
+++ typedoc.config.js
@@ -0,0 +1,26 @@
+
+import pkg from "./package.json" with {type: "json"}
+
+
+export default {
+	githubPages: true,
+	navigationLinks: {
+		Github: pkg.repository,
+		Issues: `${pkg.repository}/issues`,
+		npm: `http://npmjs.com/${pkg.name}`,
+	},
+	readme: "README.md",
+	logLevel: "Verbose",
+	entryPoints: [
+		"src/**/*.ts"
+	],
+	out: "docs",
+	excludePrivate: true,
+	excludeExternals: true,
+	validation: {
+		invalidLink: true,
+	},
+	projectDocuments: [
+		// "docs-src/DEVELOPMENT.md",
+	]
+}
